89f2ae6731e12d28498db7543793c8e6bc28dc3e
gromdimon <grom.dima.grom@gmail.com>
2023-05-23 09:54:29 +0200

    2a

--- src/ue5_dzima/auf2a.cpp
+++ src/ue5_dzima/auf2a.cpp
@@ -6,40 +6,65 @@ Code von gromdimon
 //**********************************************************************************************
 // AUFGABE 2a
 
+// Wir habe in der Vorlesung eine Möglichkeit kennengelernt das Minimum einer
+// Liste zu bestimmen. In dieser Aufgabe geht es um eine alternative Möglichkeit
+// dafür. Es soll ein Turniermodus betrachtet werden. Dazu werden in der ersten
+// Runde, immer zwei benachbarte Zahlen miteinander verglichen. Die kleinere
+// „gewinnt“ und kommt in die nächste Runde. In dieser Art und Weise werden die
+// Zahlen weiter verglichen, bis nach der letzten Runde nur noch eine Zahl übrig
+// ist.
+// (a) Implementieren Sie die oben beschriebene Möglichkeit das Minimum zu
+// finden.
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int findMinimum(std::vector<int> &nums) {
-  int n = nums.size();
+int findMinimum(vector<int> nums) {
+  int size = nums.size();
+  int i = 0;
+  int j = 1;
 
-  while (n > 1) {
-    int round = n / 2;
-
-    for (int i = 0; i < round; i++) {
-      int j = 2 * i;
-
-      if (nums[j] > nums[j + 1]) {
-        nums[i] = nums[j + 1];
-      } else {
-        nums[i] = nums[j];
-      }
+  while (size > 1) {
+    if (nums[i] < nums[j]) {
+      nums.erase(nums.begin() + j);
+    } else {
+      nums.erase(nums.begin() + i);
     }
-
-    if (n % 2 != 0) {
-      nums[round] = nums[n - 1];
-      round++;
-    }
-
-    n = round;
+    size--;
   }
 
   return nums[0];
 }
 
+vector<int> alternativeFindMinimum(vector<int> nums) {
+  int size = static_cast<int>(nums.size());
+
+  if (size == 1) {
+    return nums;
+  } else if (size == 2) {
+    return nums[0] < nums[1] ? vector<int>{nums[0]} : vector<int>{nums[1]};
+  } else {
+    vector<int> numsleft(nums.begin(), nums.begin() + size / 2);
+    vector<int> numsright(nums.begin() + size / 2, nums.end());
+    vector<int> minimumLeft = alternativeFindMinimum(numsleft);
+    vector<int> minimumRight = alternativeFindMinimum(numsright);
+    return alternativeFindMinimum(
+        minimumLeft[0] < minimumRight[0] ? minimumLeft : minimumRight);
+  }
+}
+
 int main() {
   vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+
+  cout << "Die Eingabeliste: "
+       << "\n1, 2, 3, 4, 5, 6, 7" << endl;
+
+  cout << "Das Minimum: " << endl;
   cout << findMinimum(nums) << endl;
+
+  cout << "Das Minimum (alternative): " << endl;
+  cout << alternativeFindMinimum(nums)[0] << endl;
 
   return 0;
 }
