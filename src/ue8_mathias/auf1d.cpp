262cabd7624c643b5b1522f939815e4491f2d740
mathiashusted <username@users.noreply.github.com>
2023-06-08 19:52:53 +0200

    Sync

--- src/ue8_mathias/auf1d.cpp
+++ src/ue8_mathias/auf1d.cpp
@@ -4,19 +4,55 @@
 Wir betrachten die folgende Klasse, die den Anfang einer Implementierung eines Binärbaums enthält.
 Die Größe des Binarbaums wird beim Aufruf des Konstruktors definiert, und genau so viel
 Speicherplatz wird dann im Heap reserviert. Dieser muss am Ende (Destruktor) wieder freigegeben
-werden.
+werden. Passiert das nicht, bleibt der Speicherplatz für den Binärbaum reserviert, obwohl er nicht mehr gebraucht wird.
+
+Zudem muss ein Copy Konstruktor deklariert werden, damit im Falle einer Kopie das Verhalten bestimmt werden kann, und
+ggf. Speicherplatz alloziert werden kann.
+
+Beim Copy-Assignment-Operator geht es vor allem darum, nach dem Kopievorgang den alten Speicherplatz wieder freizugeben.
 */
 class binTree {
     private:
         int* tree;
+        int size;
         int position;
     public:
         binTree(int size) {
-            tree = new int(size);
-            position = 0;
+            this -> tree = new int(size);
+            this -> size = size;
+            this -> position = 0;
+        }
+        binTree(const binTree & argTree) {
+            this -> tree = new int(argTree.size);
+            this -> size = argTree.size;
+            this -> position = argTree.position;
+
+            for (int i = 0; i < this->size; i++) {
+                this -> tree[i] = argTree.tree[i];
+            }
+        }
+        binTree& operator=(const binTree & argTree) {
+            if (this != &argTree) {
+
+                delete[] tree;
+
+                this->tree = new int(argTree.size);
+                this->position = argTree.position;
+                this->size = argTree.size;
+
+                for (int i = 0; i < this->size; i++) {
+                    this -> tree[i] = argTree.tree[i];
+                }
+            }
+            return *this;
         }
         ~binTree() {
-            tree = nullptr;
+            delete tree;
             position = 0;
+            size = 0;
         }
 };
+
+int main() {
+
+}
---
+++ src/ue8_mathias/auf2a.cpp
@@ -0,0 +1,26 @@
+// Gruppe M. Husted, A. Malze, S. Kutscher
+
+#include <iostream>
+#include <string>
+
+class Escooter {
+    private:
+        unsigned ps;
+        unsigned gewicht;
+        std::string farbe;
+        std::string marke;
+    public:
+        Escooter(unsigned aps, unsigned agewicht, std::string afarbe, std::string amarke) {
+
+        }
+        ~Escooter() {
+            
+        }
+        void motorAufruesten(unsigned newPs) {
+            
+        }
+};
+
+int main() {
+
+}
